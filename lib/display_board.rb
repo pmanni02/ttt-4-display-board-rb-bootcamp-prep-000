f11c10f51fe623d8c5cd4d0440100ebaafd3d1fe
Phoebe Manning <phoebe.manning8@gmail.com>
2017-09-22 19:47:32 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(b)
-  puts "               #{b[0]} |   |
-                     -----------
-                        |   |
-                     -----------
-                        |   |   "
+  puts "    #{b[0]} |   |
+        -----------
+           |   |
+        -----------
+           |   |   "
 end
